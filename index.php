40569084065a096c435413cacfb70f205e6cb5bc
Sourabh Rupani <saurabhrupani@gmail.com>
2017-10-06 20:57:21 +0530

    added all files

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Environs Movement</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="container-fluid">
+        <div class="header-left">
+          <img class="logo" src="" alt="logo">
+        </div>
+        <div class="header-right">
+          <a href="login.php" class="login">Log in</a>
+        </div>
+      </div>
+    </header>
+    <div class="top-wrapper">
+    	<div class="container-fluid">
+        
+    	</div>
+    	</div>
+    <div class="lesson-fluid">
+      	<div class="container-fluid">
+        	<div id="slideshow">
+   				<div>
+     				<img src="">
+   				</div>
+   				<div>
+     				<img src="">
+				</div>
+   			<div>
+   	  	</div>
+	</div>
+    <div class="message-wrapper">
+      	<div class="container-fluid">
+        	<div class="heading">
+        		<h2>Ready to know more about Eco-friendly inventions</h2>
+          		<h3>If you are ready ,Press the button below!!!!</h3>
+			</div>
+        	<span class="btn message">Sign Up</span>
+      	</div>
+    </div>
+    <footer>
+      <div class="container-fluid">
+      	<img class="footer-logo" src="" alt="logo">
+		<ul class="list">
+			<li><a href="#">About</a></li>
+			<li><a href="#">Gallery</a></li>
+			<li><a href="#">Contact Us</a></li>
+		</ul> 
+      </div>
+    </footer>
+  </body>
+</html>
